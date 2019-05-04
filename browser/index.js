217d1b3183ffced6616c0b744284c95afba3f81b
Monica Barboza <barboza.monica2@gmail.com>
2019-05-04 13:36:28 -0500

    cycle-1 complete

--- browser/index.js
+++ browser/index.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+
 console.log('Hello Webpack!');
+
+ReactDOM.render(<Main />, document.getElementById('app'));
+
+
